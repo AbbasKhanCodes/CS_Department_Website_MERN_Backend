4148b9be9e8f9baab6ffff2da95040d5bc9e397c
AbbasKhanCodes <137089493+AbbasKhanCodes@users.noreply.github.com>
2023-08-24 10:02:18 +0500

    Commented News backend data from getting.
    Now News data will not be fetched from backend.

--- controllers/getAllData.js
+++ controllers/getAllData.js
@@ -1,14 +1,14 @@
 import Event from "../models/event.js";
-import News from "../models/news.js";
+//import News from "../models/news.js";
 const getAllData = async (req, res) => {
   try {
-    const events = await Event.find().sort({ date: 1 });
-    const news = await News.find().sort({ date: -1 });
+    const events = await Event.find().sort({ date: -1 });
+    // const news = await News.find().sort({ date: -1 });
 
     // Send the response
     res.status(201).json({
       message: "Data has displayed successfully!",
-      news,
+      //news,
       events,
     });
   } catch (err) {
